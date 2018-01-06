14302a9b14c02d03f5033785685faa6107c96f47
John Marocco <fauster12@gmail.com>
2018-01-07 00:00:54 +0100

    added dummy GameplayScreen

---
+++ core/src/com/mygdx/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package com.mygdx.screens;
+
+import com.mygdx.game.TutorialClickerGame;
+
+public class GameplayScreen extends AbstractScreen{
+
+	public GameplayScreen(TutorialClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+
+	}
+
+}
