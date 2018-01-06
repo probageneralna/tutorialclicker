aa60a6e23a5921db670164fed38cef03fa09015b
John Marocco <fauster12@gmail.com>
2018-01-07 00:24:37 +0100

    Displaying Player Image

--- core/src/com/mygdx/screens/GameplayScreen.java
+++ core/src/com/mygdx/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.screens;
 
+import com.mygdx.entities.Player;
 import com.mygdx.game.TutorialClickerGame;
 
 public class GameplayScreen extends AbstractScreen{
+
+	private Player player;
 
 	public GameplayScreen(TutorialClickerGame game) {
 		super(game);
@@ -10,7 +13,25 @@ public class GameplayScreen extends AbstractScreen{
 	}
 
 	private void init() {
+		initPlayer();
+	}
 
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player); //dodanie aktora do sceny
+	}
+
+	public void render(float delta) {
+		super.render(delta); //super, czyli wszystko to, co w abstarcyjnym screenie
+		update(); //najpoierw update przez rysowaniem
+
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act(); //czyli rob update co kzda klatke dla wszystkich aktorow na scenie
 	}
 
 }
