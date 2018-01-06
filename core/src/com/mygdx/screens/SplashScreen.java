b6957c63d8040596b067424411bcdd6f3d8d5491
John Marocco <fauster12@gmail.com>
2018-01-07 00:07:28 +0100

    Change to GameplayScreen after 1s

--- core/src/com/mygdx/screens/SplashScreen.java
+++ core/src/com/mygdx/screens/SplashScreen.java
@@ -1,15 +1,25 @@
 package com.mygdx.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.mygdx.game.TutorialClickerGame;
 
 public class SplashScreen extends AbstractScreen{
 
 	private Texture splashImg;
 
-	public SplashScreen(TutorialClickerGame game) {
+	public SplashScreen(final TutorialClickerGame game) { //finalne, bo jezeli przekazujemy cos do taksa to musi miec pewnosc, ze to sie nie zmieni
 		super(game);
 		init();
+
+		Timer.schedule(new Task() {
+
+			@Override
+			public void run() {
+				game.setScreen(new GameplayScreen(game)); //przejscie na nowy screen
+			}
+		}, 1); //task wykona sie po 1 sekundzie
 	}
 
 	private void init() {
