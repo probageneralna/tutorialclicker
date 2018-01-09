4c96cfd318e1c5c2f1b1a6edf0659ac324d5f037
John Marocco <fauster12@gmail.com>
2018-01-09 20:56:39 +0100

    saving player score

--- core/src/com/mygdx/game/TutorialClickerGame.java
+++ core/src/com/mygdx/game/TutorialClickerGame.java
@@ -1,9 +1,14 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.mygdx.screens.SplashScreen;
 
 public class TutorialClickerGame extends Game {
+
+	public final static String GAME_PREFS = "pl.javadevmatt.preclicker.pref";
+	public final static String GAME_SCORE = "pl.javadevmatt.preclicker.pref.score";
 
 	public final static String GAME_NAME = "Tutorial Clicker";
 	public final static int WIDTH = 480;
@@ -11,15 +16,29 @@ public class TutorialClickerGame extends Game {
 
 	private boolean paused; //czy gra jest zapausowana, nazwano to flag¹\
 
+	private Preferences prefs; //mozna tu zapisac np punkty
+
 	private int points; //zliczanie punktow
 
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this)); //splashScreen do ktorej przekazujemy ta klase Game
+	}
+
+	private void init() {
+		prefs = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points = prefs.getInteger(GAME_SCORE); //aby wczytywac dane po zamknieciu aplikacji
 	}
 
 	public void addPoint() {
 		points++;
+		prefs.putInteger(GAME_SCORE, points);
+		prefs.flush(); //musi byc, zeby zapisac w pamieci
 	}
 
 	public boolean isPaused() {
