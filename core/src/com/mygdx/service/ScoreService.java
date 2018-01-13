549f7ae1b0b9c5dc7257e5b85ee0faff94140d94
John Marocco <fauster12@gmail.com>
2018-01-13 19:44:52 +0100

    added passice income field to ScoreService

--- core/src/com/mygdx/service/ScoreService.java
+++ core/src/com/mygdx/service/ScoreService.java
@@ -7,10 +7,12 @@ public class ScoreService {
 
 	public final static String GAME_PREFS = "pl.javadevmatt.preclicker.pref";
 	public final static String GAME_SCORE = "pl.javadevmatt.preclicker.pref.score";
+	public final static String GAME_PASSIVE_INCOME = "pl.javadevmatt.preclicker.pref.passive";
 
 	private Preferences prefs; // mozna tu zapisac np punkty
 
 	private int points; // zliczanie punktow
+	private int passiveIncome;
 
 	public ScoreService() {
 		init();
@@ -19,6 +21,7 @@ public class ScoreService {
 	private void init() {
 		prefs = Gdx.app.getPreferences(GAME_PREFS);
 		loadScore();
+		loadPassiveIncome();
 	}
 
 	private void loadScore() {
@@ -26,28 +29,34 @@ public class ScoreService {
 												// zamknieciu aplikacji
 	}
 
+	private void loadPassiveIncome() {
+		passiveIncome = prefs.getInteger(GAME_PASSIVE_INCOME);
+	}
+
 	public void addPoints(int pointsToAdd) {
 		points += pointsToAdd; // dodane punkty przekazanen w parametrze
-		updateSavedScoreInPrefs();
+		updateSavedScoreAndPassiveIncomeInPrefs();
 	}
 
 	public void addPoint() {
 		points++;
-		updateSavedScoreInPrefs();
+		updateSavedScoreAndPassiveIncomeInPrefs();
 	}
 
 	public void resetGameScore() {
 		points = 0;
-		updateSavedScoreInPrefs();
+		updateSavedScoreAndPassiveIncomeInPrefs();
 	}
 
-	private void updateSavedScoreInPrefs() {
+	private void updateSavedScoreAndPassiveIncomeInPrefs() {
 		prefs.putInteger(GAME_SCORE, points);
+		prefs.putInteger(GAME_PASSIVE_INCOME, passiveIncome);
 		prefs.flush();// musi byc, zeby zapisac w pamieci
 	}
 
 	public void addPassiveIncome() {
-		// TODO implement
+		passiveIncome++;
+		updateSavedScoreAndPassiveIncomeInPrefs();
 	}
 
 	public int getPoints() {
